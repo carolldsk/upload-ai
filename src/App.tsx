96de638d80b15a88928a0f7ca30cb5eb92ea52e5
Ana Carolina Vieira Batista <ana.batista@estadao.com>
2023-09-13 23:32:11 -0300

    texto

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export function App() {
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
-            Desenvolvido com 💜 no NLW da Rocketseat
+            Desenvolvido com 💜
           </span>
 
           <Separator orientation="vertical" className="h-6" />
